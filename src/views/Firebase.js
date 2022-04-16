84ea6b97e11f27288c8f2a2e91ea9729a44809fb
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2022-04-16 10:20:50 +0530

    added notes page

---
+++ src/views/Firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGs_fbwAPvlvTstHiFsZVffkA1SNcJpCI",
+  authDomain: "ou-all-in-one-83b53.firebaseapp.com",
+  projectId: "ou-all-in-one-83b53",
+  storageBucket: "ou-all-in-one-83b53.appspot.com",
+  messagingSenderId: "850725617809",
+  appId: "1:850725617809:web:4c69dbf029d77fd2b83f30",
+  measurementId: "G-QTEH9XWQPQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- src/views/Notes.js
+++ src/views/Notes.js
@@ -1,14 +1,96 @@
-import React from 'react';
-// import sections
-import Hero from '../components/sections/Hero';
+import React, {useState} from 'react';
+import firebase from './Firebase';
+import classNames from 'classnames';
+import { SectionProps } from '../utils/SectionProps';
 
-const Notes = () => {
+const propTypes = {
+  ...SectionProps.types
+}
+
+const defaultProps = {
+  ...SectionProps.defaults
+}
+
+const Notes = (
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+) => {
+
+  const outerClasses = classNames(
+    'hero section center-content',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'section center-content',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const [allDocs,setAllDocs] = useState([]);
+  const db = firebase.firestore();
+
+  function fetchAll(e){
+    e.preventDefault();
+    db.collection("Contacts")
+    .get()
+    .then((snapshot)=>{
+      if(snapshot.docs.length>0){
+        snapshot.docs.forEach((doc)=>{
+          setAllDocs((prev)=>{
+            return[...prev,doc.data()];
+          });
+        });
+      }
+    });
+
+    console.log(allDocs);
+
+  }
 
   return (
     <>
+
+    <section
+      {...props}
+      className={outerClasses}
+    >
+
       <h1>Notes Page</h1>
+      <button onClick={fetchAll}></button>
+
+      <div className={innerClasses}>
+      <table text-align="center">
+      <tr>
+        <th>Name</th>
+        <th>Download</th>
+      </tr>
+      {allDocs.map(function(a) {
+       return (
+          <tr>
+            <td>{a.name}</td>
+            <td>{a.phone}</td>
+          </tr>
+       );
+      })}
+      </table>
+      </div>
+      </section>
     </>
   );
 }
+
+Notes.propTypes = propTypes;
+Notes.defaultProps = defaultProps;
 
 export default Notes;
